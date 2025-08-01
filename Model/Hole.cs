10aa734331ae80d8ef2d645bfe4306f40173eb24
OlegRybkin <o-rybkin2010@yandex.ru>
2025-08-01 17:02:32 +0300

    Работа над отверстием

--- Model/Hole.cs
+++ Model/Hole.cs
@@ -8,77 +8,144 @@ using System.Threading.Tasks;
 
 namespace HoleFramingModule.Model
 {
+    enum HostElementType
+    {
+        Floor,
+        Wall,
+        Null
+    }
+
     internal class Hole
     {
         internal FamilyInstance FamilyInstance { get; set; }
+
+        internal XYZ Location { get; private set; }
+        internal XYZ FacingOrientation { get; private set; }
+
+        internal Curve LengthCurve { get; private set; }
+        internal Curve WidthCurve { get; private set; }
+        internal double Thickness { get; private set; }
 
         internal Curve LongCurve { get; set; }
         internal Curve TransverseCurve { get; set; }
 
         private Element HostElement;
+        private HostElementType HostElementType;
 
         internal Hole(Element element)
         {
             FamilyInstance = element as FamilyInstance;
-            HostElement = ((FamilyInstance)element).Host;
 
-            GetHoleCurves();
+            HostElement = ((FamilyInstance)element).Host;
+            HostElementType = GetHostElementType();
+
+            Location = GetLocation();
+            FacingOrientation = GetFacingOrienation();
+
+            Thickness = GetThickness();
+            Location = GetLocation();
+
+
+
+
+            
         }
 
-        private List<Curve> GetHoleCurves()
+        private double GetThickness()
         {
-            XYZ direction = GetDirection();
+            double thickness = 0;
+            Document doc = FamilyInstance.Document;
 
-            Options options = new Options()
+            Element hostElementType = doc.GetElement(HostElement.get_Parameter(BuiltInParameter.ELEM_TYPE_PARAM).AsElementId());
+
+            switch (HostElementType)
             {
-                ComputeReferences = true,
-                IncludeNonVisibleObjects = true,
-            };
+                case HostElementType.Wall:
 
-            List<Curve> curves = new List<Curve>();
+                    thickness = hostElementType.get_Parameter(BuiltInParameter.WALL_ATTR_WIDTH_PARAM).AsDouble();
 
-            GeometryElement geometryElement = FamilyInstance.get_Geometry(options);
+                    break;
 
-            foreach (GeometryObject geometryObject in geometryElement)
-            {
-                if (geometryObject is GeometryInstance geometryInstance)
-                {
-                    GeometryElement geoElement = geometryInstance.GetInstanceGeometry();
+                case HostElementType.Floor:
 
-                    foreach (GeometryObject geoObject in geoElement)
-                    {
-                        if (geoObject is Line)
-                        {
-                            curves.Add((Curve)geoObject);
-                        }
-                    }
-                }
+                    thickness = hostElementType.get_Parameter(BuiltInParameter.FLOOR_ATTR_DEFAULT_THICKNESS_PARAM).AsDouble();
+
+                    break;
+            }
+            
+            return thickness;
+        }
+
+        private XYZ GetLocation()
+        {
+            XYZ location = XYZ.Zero;
+
+            switch (HostElementType) 
+            { 
+                case HostElementType.Floor:
+
+                    location = ((LocationPoint)FamilyInstance.Location).Point;
+
+                    break;
+
+                case HostElementType.Wall:
+
+                    location = new XYZ
+                        (
+                            ((LocationPoint)FamilyInstance.Location).Point.X + FacingOrientation.X * 0.5 * Thickness,
+                            ((LocationPoint)FamilyInstance.Location).Point.Y + FacingOrientation.Y * 0.5 * Thickness,
+                            ((LocationPoint)FamilyInstance.Location).Point.Z
+                        );
+
+                    break;
             }
 
-            return curves;
+            return location;
         }
 
-
-        private XYZ GetDirection()
+        private XYZ GetFacingOrienation()
         {
-            XYZ direction = XYZ.Zero;
+            XYZ facingOrientation = XYZ.Zero;
 
             switch (HostElement.get_Parameter(BuiltInParameter.ELEM_CATEGORY_PARAM).AsValueString())
             {
                 case "Перекрытия":
 
-                    direction = new XYZ(0, 0, 1);
+                    FacingOrientation = new XYZ(0, 0, 1);
 
                     break;
 
                 case "Стены":
 
-                    direction = ((Wall)HostElement).Orientation;
+                    FacingOrientation = ((Wall)HostElement).Orientation;
 
                     break;
             }
 
-            return direction;
+            return facingOrientation;
         }
+
+        private HostElementType GetHostElementType()
+        {
+            HostElementType hostElementType = HostElementType.Null;
+
+            switch (HostElement.get_Parameter(BuiltInParameter.ELEM_CATEGORY_PARAM).AsValueString())
+            {
+                case "Перекрытия":
+
+                    HostElementType = HostElementType.Floor;
+
+                    break;
+
+                case "Стены":
+
+                    HostElementType = HostElementType.Wall;
+
+                    break;
+            }
+
+            return hostElementType;
+        }
+
     }
 }
